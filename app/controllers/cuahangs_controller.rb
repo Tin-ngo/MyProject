28c0d3259f8f102ed1e752943dfcd89a8d697c4a
Tin-ngo <70043077+Tin-ngo@users.noreply.github.com>
2021-05-29 09:59:31 +0700

    MyProject ruby on rails

--- app/controllers/cuahangs_controller.rb
+++ app/controllers/cuahangs_controller.rb
@@ -1,11 +1,32 @@
 class CuahangsController < ApplicationController
 
+  PRODUCT_PER_PAGE = 4
 
   def index
+    # @product_all = Product.all
   	@danhmuc_all = Danhmuc.all
-  	# @product_all = Product.all
+
+# đang làm chỗ page và tìm kiếm
+    @page = params.fetch(:page, 0).to_i
     @q = Product.ransack(params[:q])
-    @product_all = @q.result
+
+    @sort_order = params[:sort_by]
+    if @sort_order == "tang"
+    @product_all = @q.result.offset(@page * PRODUCT_PER_PAGE).limit(PRODUCT_PER_PAGE).order(dongia: :asc)
+    elsif @sort_order == "giam"
+    @product_all = @q.result.offset(@page * PRODUCT_PER_PAGE).limit(PRODUCT_PER_PAGE).order(dongia: :desc)
+    else
+      @product_all = @q.result.offset(@page * PRODUCT_PER_PAGE).limit(PRODUCT_PER_PAGE)
+    end
+  end
+
+  def self.order_list(sort_order)
+    if sort_order == "asc"
+      order(dongia: :asc)
+    elsif sort_order == "desc"
+      order(dongia: :desc)
+    end
+      
   end
 
 
@@ -14,8 +35,8 @@ class CuahangsController < ApplicationController
     @q = Product.ransack(params[:q])
     @product_all = @q.result
     @product = Product.find(params[:id])
-
   end
+
 
  
 
